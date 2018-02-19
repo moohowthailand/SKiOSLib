6b0971b9843cc26f73c5f9b8f0cf2b5cccbb2dad
Soemsak Loetphornphisit <soemsakplus@gmail.com>
2018-02-19 14:52:10 +0700

    Breeze - add pod files

---
+++ Example/Pods/Target Support Files/Pods-SKiOSLib_Example/Pods-SKiOSLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SKiOSLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SKiOSLib_Example
+@end
---
+++ Example/Pods/Target Support Files/SKiOSLib/SKiOSLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SKiOSLib : NSObject
+@end
+@implementation PodsDummy_SKiOSLib
+@end
